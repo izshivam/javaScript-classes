30145ed36b9b131a3ace77616b679005d8449420
izshivam <shivamkumarchoudhary8116@gmail.com>
2023-09-28 13:03:02 +0530

    numbers & maths completed in javascript

---
+++ 01_basics/07_numbers.js
@@ -0,0 +1,20 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+console.log(balance.toFixed(1));
+
+//const otherNumber = 123.896
+//console.log(otherNumber.toPrecision(3));
+
+const otherNumber = 23.896
+console.log(otherNumber.toPrecision(3));
+
+ 
+const hundreds = 1000000
+//console.log(hundreds.toLocaleString());
+console.log(hundreds.toLocaleString('en-IN'));
